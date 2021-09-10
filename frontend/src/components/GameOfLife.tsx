d73f1107a70508e95464bd259cfa309ae82b2d8f
Shunya Endo <shunyaendoh.bz@gmail.com>
2021-09-10 13:42:38 +0900

    🔧 fix: fix

--- frontend/src/components/GameOfLife.tsx
+++ frontend/src/components/GameOfLife.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import tw, { styled } from 'twin.macro'
 
 interface Props {}
 
 const GameOfLife: React.FC<Props> = () => {
-  const ref = useRef(null)
   useEffect(() => {
     init()
-  }, [ref])
+  }, [])
 
   const init = async () => {
     const { Universe } = await import('@esh2n/wasm-game-of-life')
+    const pre = document.getElementById('game-of-life-canvas')
+
     const universe = Universe.new()
     const renderLoop = () => {
-      ref.current = universe.render()
+      pre.textContent = universe.render()
       universe.tick()
       requestAnimationFrame(renderLoop)
     }
@@ -21,14 +22,14 @@ const GameOfLife: React.FC<Props> = () => {
     requestAnimationFrame(renderLoop)
   }
 
-  const GameWrapper = styled.div`
-    ${tw`absolute flex flex-col bg-indigo-500 justify-center items-center w-12 h-12 my-10 mx-0`}
+  const GameWrapper = styled.pre`
+    ${tw`absolute flex flex-col bg-indigo-500 justify-center items-center w-96 h-96 my-10 mx-0`}
   `
 
   return (
     <>
       GameOfLife
-      <GameWrapper ref={ref} />
+      <GameWrapper id="game-of-life-canvas" />
     </>
   )
 }
